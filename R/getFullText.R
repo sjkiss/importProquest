0cd5481d6a70d671fee4bf1ae3379f673ab64764
sjkiss <sjkiss@gmail.com>
2026-06-08 12:04:10 -0400

    Changed get full text

--- R/getFullText.R
+++ R/getFullText.R
@@ -1,7 +1,7 @@
 getFullText<-function(x){
   if(any(grepl('Full [Tt]ext:', x))) {
   start <- grep('Full [Tt]ext:', x)
-  end <- grep(':', x)
+  end <- grep('(^CAPTION:)|(^Subject:)|(^Title:)', x)
   end <- end[which(end > start)[1]] - 1
   return(gsub("Full [Tt]ext:", '', x[start:end]))
   } else {
